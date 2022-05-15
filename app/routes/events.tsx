2ad929d6c4887e00ed0f85ef8459a1c1366cac32
Vadim <vadim@Vadims-MacBook-Pro.local>
2022-05-15 14:15:59 -0800

    fixed line-height

--- app/routes/events.tsx
+++ app/routes/events.tsx
@@ -1,11 +1,11 @@
 import { Outlet } from 'remix';
 
-const header = {
-  fontFamily: 'Impact',
-  fontSize: 50,
-  color: '#233F61',
-  textAlign: 'left' as const, // Had to declare it like this or would get error
-};
+// const header = {
+//   fontFamily: 'Impact',
+//   fontSize: 50,
+//   color: '#233F61',
+//   textAlign: 'left' as const, // Had to declare it like this or would get error
+// };
 
 export default function Events() {
   return (
